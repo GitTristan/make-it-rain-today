6c5e723bc6ceb4ae6ab31f14746d968dc7b3424f
Tristan Rubadeau <tristan@rubadeau.com>
2015-04-09 10:43:30 -0600

    testing pushing up to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'slack_hook', to: 'slack_webhooks#new'
+  get 'stripe_hook', to: 'stripe_webhooks#new'
+
 
   resources :users do
     resources :plans
