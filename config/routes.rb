a5fedc005e4c9701e5e272240ae861592223c90a
Tristan Rubadeau <tristan@rubadeau.com>
2015-04-08 21:55:49 -0600

    worked through onmiauth troubles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-  
+
   root 'welcome#index'
 
   get 'slack_hook', to: 'slack_webhooks#new'
